284925a6a0c86d97cfe656356f827d91e9f73511
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-21 13:42:16 -0700

    Finished the standard deviation

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -9,3 +9,24 @@ ages.each do |number|
   sum_of_all_nums += number
   num_of_integers += 1
 end
+
+average = sum_of_all_nums.to_f / num_of_integers.to_f
+
+new_array_nums_minus_average = []
+ages.each do |number|
+  new_array << (number - average)
+end
+
+new_array_nums_squared = []
+new_array_nums_minus_average.each do |number|
+  number = x
+  new_array_nums_squared << (number * x)
+end
+
+new_sum = 0
+new_array_nums_squared.each do |number|
+  new_sum += number
+end
+
+number = new_sum / num_of_integers
+result = sqrt(number)
