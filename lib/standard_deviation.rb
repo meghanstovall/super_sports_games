4a50c9f1cf3e3ec312cd2d41deb7a58d1a202d4e
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-21 13:31:30 -0700

    Initial commit

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,11 @@
 ages = [24, 30, 18, 20, 41]
 
 # Your code here for calculating the standard deviation
-
 # When you find the standard deviation, print it out
+
+sum_of_all_nums = 0
+num_of_integers = 0
+ages.each do |number|
+  sum_of_all_nums += number
+  num_of_integers += 1
+end
