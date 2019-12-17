7d02d4d3c8668a091e47e3a583ee9fd27db031b1
Meghan Stovall <meghanstovall15@augustana.edu>
2019-12-17 11:20:02 -0700

    Still trying to print out the event summary

--- lib/games.rb
+++ lib/games.rb
@@ -19,19 +19,20 @@ class Games
   end
 
   def headers
-    puts "Event".ljust(15) +
-    "Max Age".ljust(20) +
-    "Min Age".ljust(20) +
-    "Average Age".ljust(20) +
-    "StdDev Age" + "\n"
+    event_header = "Event".ljust(15)
+    max_age_header = "Max Age".ljust(20)
+    min_age_header = "Min Age".ljust(20)
+    average_header = "Average Age".ljust(20)
+
+    puts "#{event_header} #{max_age_header} #{min_age_header} #{average_header} StdDev Age"
   end
 
   def event_summary(event)
-    puts "#{event.name.ljust(15)}" +
-    "#{event.max_age.to_s.ljust(20)}" +
-    "#{event.min_age.to_s.ljust(20)}" +
-    "#{event.average_age.to_s.ljust(20)}" +
-    "#{event.standard_deviation_age.to_s}"
+    puts "#{event.name.ljust(15)}
+    #{event.max_age.to_s.ljust(20)}
+    #{event.min_age.to_s.ljust(20)}
+    #{event.average_age.to_s.ljust(20)}
+    #{event.standard_deviation_age.to_s}"
   end
 
   def all_events_summary
